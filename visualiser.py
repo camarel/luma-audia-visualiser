abace0035c43bc5b47bc6df6967db85447253ae8
Claudio Marelli <camarelli@gmx.net>
2020-02-13 11:09:27 +0000

    flake8 code clean up

--- visualiser.py
+++ visualiser.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright (c) 2020 Claudio Marelli
 # See LICENSE.txt for details
 
-import time
 import sys
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
+
 
 class Visualizer():
     def __init__(self):
@@ -17,6 +17,7 @@ class Visualizer():
         self.device = max7219(serial, cascaded=4, block_orientation=-90)
         self.device.contrast(2)
 
+        print('initialized device')
 
     def draw(self, nums):
         with canvas(self.device) as draw:
@@ -25,21 +26,28 @@ class Visualizer():
                 if num >= 0:
                     draw.line((i, 0, i, num), fill="red")
 
-
     def handle_stdin(self):
         while True:
             try:
-                nums = map(int, sys.stdin.readline()[:-2].split(';'))
+                # read line without last two chars
+                line = sys.stdin.readline()[:-2]
+                nums = map(int, line.split(';'))
                 self.draw(nums)
 
             except Exception as e:
                 print(e)
 
+    def close(self):
+        self.device.hide()
+        print('closing device')
+
 
 if __name__ == "__main__":
+    visualizer = Visualizer()
+
     try:
-        visualizer = Visualizer()
         visualizer.handle_stdin()
 
     except KeyboardInterrupt:
         pass
+        visualizer.close()
